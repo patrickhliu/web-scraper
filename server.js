5de4825b7a696867e81f11e9837f8ebeffdf9c9f
Patrick Liu <patrick.h.liu@gmail.com>
2025-10-01 22:01:34 -0700

    scrape w/ playwright

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // https://dev.to/drruvari/building-a-web-scraper-with-reactjs-express-and-tailwindcss-a-journey-into-data-collection-31g9
 // https://books.toscrape.com/
 
+
+const playwright = require("playwright")
 const express = require("express");
 const axios = require("axios");
 const cheerio = require("cheerio");
@@ -28,12 +30,32 @@ app.get('/', (req, res, next) => {
   res.send('Hello World from Express!');
 });
 
+app.get('/open-chrome', async (req, res, next) => {
+    console.log('open-chrome');
+
+    const browser = await playwright.chromium.launch();
+
+      const page = await browser.newPage();
+
+      await page.goto('https://www.nintendo.com/us/search/#p=1&cat=gme&sort=df');
+
+    const headings = await page.locator('div.Duonm');
+
+    for (let i = 0; i < await headings.count(); i++) {
+        console.log(await headings.nth(i).innerText());
+    }
+
+      await browser.close();
+});
+
 app.get('/scrape', (req, res, next) => {
   //const url = req.query.url;
-  const url = 'https://www.nintendo.com/us/store/sales-and-deals';
+  //const url = 'https://www.nintendo.com/us/store/sales-and-deals';
+  const url = 'https://www.nintendo.com/us/search/#p=1&cat=gme&sort=df';
 
   try {
     axios.get(url).then(response => {
+        console.log(response);
       /* console.log(response.data);
       console.log(response.status);
       console.log(response.statusText);
@@ -44,9 +66,11 @@ app.get('/scrape', (req, res, next) => {
         const $ = cheerio.load(html);
 
         let htmlStr = $('script[id="__NEXT_DATA__"]').text();
-        let result = JSON.parse(htmlStr);
+        logger.info(htmlStr);
 
-        res.status(200).send(result.props.pageProps.page.content.merchandisedGrid);
+        //let result = JSON.parse(htmlStr);
+        //console.log(result.props.pageProps);
+        //res.status(200).send(result.props.pageProps.page.content.merchandisedGrid);
 
         /* for(let k in result.props.pageProps.page.content.merchandisedGrid) {
           console.log(k, result.props.pageProps.page.content.merchandisedGrid[k].name);
