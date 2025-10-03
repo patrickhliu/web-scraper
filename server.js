84cf7b82eb83185c670293a7e63af114aefd765f
Patrick Liu <patrick.h.liu@gmail.com>
2025-10-02 18:15:53 -0700

    learn more playwright api functions

--- server.js
+++ server.js
@@ -30,58 +30,79 @@ app.get('/', (req, res, next) => {
   res.send('Hello World from Express!');
 });
 
-app.get('/open-chrome', async (req, res, next) => {
-    console.log('open-chrome');
+app.get('/scrape', async (req, res, next) => {
+    //let url = 'https://www.nintendo.com/us/search/#p=1&cat=gme&sort=df';
+    let url = 'https://www.nintendo.com/us/store/games/#p=1&sort=df&show=0&f=topLevelFilters&topLevelFilters=Deals';
 
     const browser = await playwright.chromium.launch();
 
       const page = await browser.newPage();
 
-      await page.goto('https://www.nintendo.com/us/search/#p=1&cat=gme&sort=df');
+      await page.goto(url);
 
-    const headings = await page.locator('div.Duonm');
+    // A locator is not iterable per se
+    // each parentDiv has 4 child divs...
+    const parentDivs = await page.locator('div.HRRF1').all();
+    let games = [];
 
-    for (let i = 0; i < await headings.count(); i++) {
-        console.log(await headings.nth(i).innerText());
+    // await parentDiv.locator("> *").all();  -- lists the 4 child divs...
+    console.log('pat1');
+
+    for (const parentDiv of parentDivs) {
+        let firstDiv = await parentDiv.locator("> *").first().locator("> *").first().innerText();
+        let count = await parentDiv.locator("> *").first().locator("> *").count();
+        let title = 'unknown';
+        let releaseDate = 'unknown';
+
+        if(count == 2) { // not discounted
+            title = await parentDiv.locator("> *").first().locator("> *").first().innerText();
+            releaseDate = await parentDiv.locator("> *").first().locator("> *").nth(1).innerText();
+        }
+
+        games.push({title: title, release_date: releaseDate.split(' ')[1]});
+        continue;
+
+        //let firstDiv = await parentDiv.locator('div:nth-child(1)'); // i guess this is if you have 4 sibling divs?  // J5IPV
+        //let title = await firstDiv.locator('h3').nth(0).innerText();    // sometimes it's h2 or h3....
+        //console.log('title', title);
+        //let releaseDate = await firstDiv.locator('div').nth(0).innerText();
+
+        //let thirdDiv = await parentDiv.locator('div:nth-of-type(3)');   // MVVbT
+        //console.log(thirdDiv.innerHTML())
+        //let regularPrice = await thirdDiv.locator('div').nth(1).locator('span').nth(0).allTextContents(); // nth(0) is the first span child... nests, grandchild, great-grandchild, etc....
+
+        //let salePrice = await thirdDiv.locator('div').nth(0).innerHTML();   //_8rhZP
+        //let pat = await thirdDiv.locator('div').nth(1).innerHTML();   // _5cBXA, can have 1 to 3 child elements...
+        //let pat = await thirdDiv.locator('div').nth(1).nth(0).count();  // if 1 - not on sale
+        //let pat = await thirdDiv.locator('div').nth(1).nth(1).count();    // if > 1 - has a discount?
+        //console.log(thirdDiv.innerHTML());
+
+
+        games.push({title: title, release_date: releaseDate, regular_price: regularPrice[0].split(':')[1]});
+        continue;
+
+
+        const childDivs = await parentDiv.locator('>div').all();    // .all() if you want to loop through it as an array...
+
+        for (const childDiv of childDivs) {
+            let count = await childDiv.locator('h3').count();
+            if(count == 0) continue;
+
+            let h3 = await childDiv.locator('h3');
+            let title = await h3.innerText();
+
+            let div = await childDiv.locator('div.k9MOS');
+            let releaseDate = await div.innerText();
+
+            games.push({title: title, releaseDate: releaseDate});
+        }
+        //let h2 = el.locate('h2["style"]="--lines: 2;"');    // --lines: 2;
+        //const textContent = await h2.textContent();
+        //console.log(`List item text: ${textContent}`);
     }
 
       await browser.close();
-});
-
-app.get('/scrape', (req, res, next) => {
-  //const url = req.query.url;
-  //const url = 'https://www.nintendo.com/us/store/sales-and-deals';
-  const url = 'https://www.nintendo.com/us/search/#p=1&cat=gme&sort=df';
-
-  try {
-    axios.get(url).then(response => {
-        console.log(response);
-      /* console.log(response.data);
-      console.log(response.status);
-      console.log(response.statusText);
-      console.log(response.headers);
-      console.log(response.config); */
-
-        const html = response.data;
-        const $ = cheerio.load(html);
-
-        let htmlStr = $('script[id="__NEXT_DATA__"]').text();
-        logger.info(htmlStr);
-
-        //let result = JSON.parse(htmlStr);
-        //console.log(result.props.pageProps);
-        //res.status(200).send(result.props.pageProps.page.content.merchandisedGrid);
-
-        /* for(let k in result.props.pageProps.page.content.merchandisedGrid) {
-          console.log(k, result.props.pageProps.page.content.merchandisedGrid[k].name);
-        } */
-      }).catch(error => {
-        console.log('error', error);
-      });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Error accessing the URL" });
-  }
+    res.send(games);
 });
 
 app.listen(port, () => {
