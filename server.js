64f96d0cc09a2895abb7042b196704d7819f8d71
Patrick Liu <patrick.h.liu@gmail.com>
2025-10-04 19:01:10 -0700

    scrape 1 page & output object response

--- server.js
+++ server.js
@@ -26,83 +26,81 @@ const logger = winston.createLogger({
   ]
 });
 
-app.get('/', (req, res, next) => {
-  res.send('Hello World from Express!');
-});
+function stripHtml(html)
+{
+   html = html.replace(/<\/?[^>]+(>|$)/g, "");
+   return html.replace(/[\n\r]/g, "");
+}
 
 app.get('/scrape', async (req, res, next) => {
     //let url = 'https://www.nintendo.com/us/search/#p=1&cat=gme&sort=df';
     let url = 'https://www.nintendo.com/us/store/games/#p=1&sort=df&show=0&f=topLevelFilters&topLevelFilters=Deals';
-
-    const browser = await playwright.chromium.launch();
-
-      const page = await browser.newPage();
-
+    let browser = await playwright.chromium.launch();
+    let page = await browser.newPage();
       await page.goto(url);
 
-    // A locator is not iterable per se
-    // each parentDiv has 4 child divs...
-    const parentDivs = await page.locator('div.HRRF1').all();
-    let games = [];
+    let results = [];
 
-    // await parentDiv.locator("> *").all();  -- lists the 4 child divs...
-    console.log('pat1');
+    let gameDivs = await page.locator('div.y83ib').all();
 
-    for (const parentDiv of parentDivs) {
-        let firstDiv = await parentDiv.locator("> *").first().locator("> *").first().innerText();
-        let count = await parentDiv.locator("> *").first().locator("> *").count();
-        let title = 'unknown';
-        let releaseDate = 'unknown';
+    for (let gameDiv of gameDivs) {
+        let photoUrl = '';
+        let title = '';
+        let releaseDate = '';
+        let currentPrice = '';
+        let regularPrice = '';
 
-        if(count == 2) { // not discounted
-            title = await parentDiv.locator("> *").first().locator("> *").first().innerText();
-            releaseDate = await parentDiv.locator("> *").first().locator("> *").nth(1).innerText();
+        try {
+            //photoUrl = await gameDiv.locator('img.UBTQd').first().getAttribute('src');
+            //console.log(photoUrl);
+        } catch (error) {
+
         }
 
-        games.push({title: title, release_date: releaseDate.split(' ')[1]});
-        continue;
+        try {
+            title = await gameDiv.locator('div.HRRF1').locator("> *").first().locator("> *").first().innerText();
+            //console.log(title);
+        } catch (error) {
 
-        //let firstDiv = await parentDiv.locator('div:nth-child(1)'); // i guess this is if you have 4 sibling divs?  // J5IPV
-        //let title = await firstDiv.locator('h3').nth(0).innerText();    // sometimes it's h2 or h3....
-        //console.log('title', title);
-        //let releaseDate = await firstDiv.locator('div').nth(0).innerText();
-
-        //let thirdDiv = await parentDiv.locator('div:nth-of-type(3)');   // MVVbT
-        //console.log(thirdDiv.innerHTML())
-        //let regularPrice = await thirdDiv.locator('div').nth(1).locator('span').nth(0).allTextContents(); // nth(0) is the first span child... nests, grandchild, great-grandchild, etc....
-
-        //let salePrice = await thirdDiv.locator('div').nth(0).innerHTML();   //_8rhZP
-        //let pat = await thirdDiv.locator('div').nth(1).innerHTML();   // _5cBXA, can have 1 to 3 child elements...
-        //let pat = await thirdDiv.locator('div').nth(1).nth(0).count();  // if 1 - not on sale
-        //let pat = await thirdDiv.locator('div').nth(1).nth(1).count();    // if > 1 - has a discount?
-        //console.log(thirdDiv.innerHTML());
-
-
-        games.push({title: title, release_date: releaseDate, regular_price: regularPrice[0].split(':')[1]});
-        continue;
-
-
-        const childDivs = await parentDiv.locator('>div').all();    // .all() if you want to loop through it as an array...
-
-        for (const childDiv of childDivs) {
-            let count = await childDiv.locator('h3').count();
-            if(count == 0) continue;
-
-            let h3 = await childDiv.locator('h3');
-            let title = await h3.innerText();
-
-            let div = await childDiv.locator('div.k9MOS');
-            let releaseDate = await div.innerText();
-
-            games.push({title: title, releaseDate: releaseDate});
         }
-        //let h2 = el.locate('h2["style"]="--lines: 2;"');    // --lines: 2;
-        //const textContent = await h2.textContent();
-        //console.log(`List item text: ${textContent}`);
+
+        try {
+            releaseDate = await gameDiv.locator('div.HRRF1').locator("> *").first().locator("> *").nth(1).innerText();
+            releaseDate = releaseDate.replace('Releases ', '');
+            //console.log(releaseDate);
+        } catch (error) {
+
+        }
+
+        try {
+            currentPrice = await gameDiv.locator('div.HRRF1').locator("> *").nth(2).locator("> *").first().locator("> *").first().locator("> *").first().innerText();
+            currentPrice = currentPrice.split(':').length > 1 ? stripHtml(currentPrice).split(':')[1] :currentPrice;
+            //console.log(currentPrice);
+        } catch (error) {
+
+        }
+
+        try {
+            regularPrice = await gameDiv.locator('div.HRRF1').locator("> *").nth(2).locator("> *").first().locator("> *").first().locator("> *").nth(1).innerText();
+            regularPrice = regularPrice.split(':').length > 1 ? stripHtml(regularPrice).split(':')[1] :regularPrice;
+            //console.log(regularPrice);
+        } catch (error) {
+
+        }
+
+        results.push({
+            photo_url: photoUrl,
+            title: title,
+            release_date: releaseDate,
+            current_price: currentPrice,
+            regular_price: regularPrice,
+            is_discount: currentPrice != regularPrice,
+            is_preorder: new Date(releaseDate) > new Date(),
+        });
     }
 
       await browser.close();
-    res.send(games);
+    res.send(results);
 });
 
 app.listen(port, () => {
